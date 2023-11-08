c34c7d604d083a77f017a9e902dc861397c36ffb
Joo <rud167637@gmail.com>
2023-11-09 03:05:16 +0900

    metadata 수정정:

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,8 @@ import { ChakraProvider, ToastProvider } from '@chakra-ui/react';
 export const metadata = {
   title: '가자지구의 피난민들에게 응원을 전해주세요',
   description: 'cheering for GAZA',
+  httpEquiv: 'Content-Security-Policy',
+  content: 'upgrade-insecure-requests',
 };
 
 export default function RootLayout({ children }) {
