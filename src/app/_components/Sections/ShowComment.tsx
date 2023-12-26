72f1bddce1db7e40ad1bd363a9d32d3753419ee9
osohyun0224 <osohyun0224@naver.com>
2023-12-26 15:40:07 +0900

    ✨ feat: 무한 스크롤 첫 로드시 20개 목록 get

--- src/app/_components/Sections/ShowComment.tsx
+++ src/app/_components/Sections/ShowComment.tsx
@@ -1,41 +1,103 @@
 'use clients';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { FaSearch } from 'react-icons/fa';
-
 import { dataTypes, propsTypes } from '@/types';
-
 import Main_firefly from '@/assets/svg/Main_firefly.jpg';
 import MessageComponent from '@/app/_components/MessageComponent';
-
+import { fetchComments, fetchGetMessageCount } from '@/utils/api';
 import Section from './section.style';
 
-const AddClassSortOn = (str: string): void => {
-  document.querySelectorAll('.sort').forEach(e => {
-    if (e.innerHTML === str) {
-      e.classList.add('SortOn');
-      return;
-    }
-    e.classList.remove('SortOn');
-  });
-};
-
-const ShowComment = ({
-  messages,
-  fetchMessage,
-  ShowCommentText,
-}: propsTypes.ShowCommentPropType) => {
+const ShowComment = ({ ShowCommentText }: propsTypes.ShowCommentPropType) => {
   const [searchInput, setSearchInput] = useState('');
+  const [comments, setComments] = useState<dataTypes.MessageType[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalMessages, setTotalMessages] = useState(0);
+  const commentWrapperRef = useRef<HTMLDivElement>(null);
 
-  const SortClick = (sort: string) => {
-    AddClassSortOn(sort);
-    fetchMessage('', sort.toLowerCase(), 100, 0);
-  };
+  // 메시지 불러오기 함수
+  const fetchMessages = useCallback(
+    async (sortType = 'new', reset = false) => {
+      if (isLoading || (!reset && currentPage * 20 >= totalMessages)) return;
+      setIsLoading(true);
+      try {
+        const newComments = await fetchComments(
+          searchInput,
+          sortType,
+          20,
+          currentPage,
+        );
+        setComments(reset ? newComments : [...comments, ...newComments]);
+        setCurrentPage(prevPage => prevPage + (reset ? 0 : 1));
+      } catch (error) {
+        console.error(error);
+      }
+      setIsLoading(false);
+    },
+    [searchInput, currentPage, isLoading, totalMessages, comments],
+  );
 
-  const onSubmitSearch = (e: React.FormEvent): void => {
-    e.preventDefault();
-    fetchMessage(searchInput, 'new', 100, 0);
-  };
+  // 초기 메시지 로드
+  useEffect(() => {
+    fetchMessages('new', true);
+  }, []);
+
+  // 총 메시지 개수 가져오기
+  useEffect(() => {
+    const getTotalMessages = async () => {
+      try {
+        const countData = await fetchGetMessageCount();
+        setTotalMessages(countData.count);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getTotalMessages();
+  }, []);
+
+  // 스크롤 이벤트 핸들러
+  const handleScroll = useCallback(() => {
+    if (!commentWrapperRef.current) return;
+
+    const { scrollTop, scrollHeight, clientHeight } = commentWrapperRef.current;
+    if (scrollHeight - scrollTop - clientHeight < 50 && !isLoading) {
+      fetchMessages();
+    }
+  }, [isLoading, fetchMessages]);
+
+  // 스크롤 이벤트 리스너 등록
+  useEffect(() => {
+    const commentWrapper = commentWrapperRef.current;
+    if (commentWrapper) {
+      commentWrapper.addEventListener('scroll', handleScroll);
+    }
+    return () => {
+      if (commentWrapper) {
+        commentWrapper.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, [handleScroll]);
+
+  // 정렬 및 검색 이벤트 핸들러
+  const SortClick = useCallback(
+    (sort: string) => {
+      setCurrentPage(1);
+      setComments([]);
+      fetchMessages(sort.toLowerCase(), true);
+    },
+    [fetchMessages],
+  );
+
+  const onSubmitSearch = useCallback(
+    (e: React.FormEvent): void => {
+      e.preventDefault();
+      setCurrentPage(1);
+      setComments([]);
+      fetchMessages('new', true);
+    },
+    [fetchMessages],
+  );
 
   const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchInput(e.target.value);
@@ -77,12 +139,11 @@ const ShowComment = ({
             </form>
           </Section.Search>
         </Section.CommentHeader>
-        <Section.ShowCommentWrapper id="CommentStack">
+        <Section.ShowCommentWrapper ref={commentWrapperRef} id="CommentStack">
           <Section.StyledScrollbar>
-            {Array.isArray(messages) &&
-              messages.map((v: dataTypes.MessageType) => (
-                <MessageComponent key={v.messageId} message={v} />
-              ))}
+            {comments.map((message: dataTypes.MessageType) => (
+              <MessageComponent key={message.messageId} message={message} />
+            ))}
           </Section.StyledScrollbar>
         </Section.ShowCommentWrapper>
       </Section.CommentWrapper>
