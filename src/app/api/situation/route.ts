b967a13428b993aa367d53b2dac8de06ad7a82f2
Joo <rud167637@gmail.com>
2024-05-29 16:21:59 +0900

    [fix] build Error : 환경변수가 원인인듯

--- src/app/api/situation/route.ts
+++ src/app/api/situation/route.ts
@@ -2,15 +2,6 @@ import instance from '@/utils/serveraxios';
 import { NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest) {
-  // 빌드 중일 때 API 호출을 방지
-  if (process.env.NODE_ENV !== 'production') {
-    return new Response(
-      JSON.stringify({
-        message: 'Skipping API call in non-production environment',
-      }),
-      { status: 200 },
-    );
-  }
   try {
     const { data } = await instance.get(`/situation`);
     return Response.json(data);
--- src/utils/serveraxios.ts
+++ src/utils/serveraxios.ts
@@ -1,9 +1,7 @@
 // src/utils/axios.js
 import axios from 'axios';
 
-const url = process.env.NEXT_PUBLIC_API_URL
-  ? process.env.NEXT_PUBLIC_API_URL
-  : 'http://localhost:3000/api/test';
+const url = 'http://localhost:3000/api/test';
 
 // Axios 인스턴스 생성
 const instance = axios.create({
