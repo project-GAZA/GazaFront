ded135e9cc11c6473d8691d30c974802e9a6d801
Joo <rud167637@gmail.com>
2024-06-14 20:12:52 +0900

    [fe:feat] Section - Middle,Eartch 반응형 배치

--- src/component/Section_WhoAmI/Section/index.tsx
+++ src/component/Section_WhoAmI/Section/index.tsx
@@ -1,11 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Earth from '@/component/Section_WhoAmI/Modules/Earth';
+import Middle from '@/component/Section_WhoAmI/Modules/Middle';
 import styles from './index.module.scss';
 
 const SectionSituation = () => {
   return (
     <div className={styles.layout}>
-      <Earth />
+      <div className={styles.middleEarth}>
+        <div className={styles.middle}>
+          <Middle />
+        </div>
+        <div className={styles.earth}>
+          <Earth />
+        </div>
+      </div>
     </div>
   );
 };
